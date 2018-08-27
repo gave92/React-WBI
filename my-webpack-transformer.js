9fe2345bae03870575dbad5bbf84734c59b96945
Marco Gavelli <marcogavelli2@gmail.com>
2018-08-28 00:05:52 +0200

    Updated webpack transformer

--- my-webpack-transformer.js
+++ my-webpack-transformer.js
@@ -1,7 +1,10 @@
 // my-webpack-transformer.js
+const paths = require('./node_modules/react-scripts/config/paths')
 module.exports = function editWebpackConfig (webpackConfig) {
   // webpackConfig is the parsed JS webpack config from react-scrips.
   // modify it here synchronously, & return it.
+  var babel = webpackConfig.module.rules[1].oneOf[1];
+  babel.include = [babel.include, paths.appNodeModules];
   return Object.assign({}, webpackConfig, {
 
   })
