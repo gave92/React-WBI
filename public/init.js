6d001eb7cf7d9c90d2f51a069ffbe4b49ce200cd
Marco Gavelli <marcogavelli2@gmail.com>
2018-09-30 11:52:51 +0200

    polyfill Object.entries

--- public/init.js
+++ public/init.js
@@ -1,1 +1,12 @@
 var __DEV__ = false;
+
+if (!Object.entries)
+   Object.entries = function( obj ){
+      var ownProps = Object.keys( obj ),
+         i = ownProps.length,
+         resArray = new Array(i); // preallocate the Array
+
+      while (i--)
+         resArray[i] = [ownProps[i], obj[ownProps[i]]];
+      return resArray;
+   };
